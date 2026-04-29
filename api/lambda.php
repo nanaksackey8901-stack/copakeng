5588fe9fca030cdccb88768c3dbc4f35ee3361b3
8901Nanakwame <nanaksackey8901@gmail.com>
2026-04-29 11:45:07 +0000

    database

--- api/lambda.php
+++ api/lambda.php
@@ -5,6 +5,20 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+$writableDirectories = [
+    '/tmp/laravel/cache',
+    '/tmp/laravel/storage/framework/cache/data',
+    '/tmp/laravel/storage/framework/sessions',
+    '/tmp/laravel/storage/framework/views',
+    '/tmp/laravel/storage/logs',
+];
+
+foreach ($writableDirectories as $directory) {
+    if (! is_dir($directory)) {
+        mkdir($directory, 0777, true);
+    }
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
